5239e608e4fe89754b064d80023f54f25588eb0c
Levi Broadnax <7132948+SystemStack@users.noreply.github.com>
2025-02-10 07:11:48 -0800

    * Updated pt schema for v1, backwards compatible with v0.19

--- Kokoro/models.py
+++ Kokoro/models.py
@@ -579,7 +579,10 @@ def build_model(path, device):
         decoder=decoder.to(device).eval(),
         text_encoder=text_encoder.to(device).eval(),
     )
-    for key, state_dict in torch.load(path, map_location='cpu', weights_only=True)['net'].items():
+    checkpoint = torch.load(path, map_location='cpu', weights_only=True)
+    if 'net' in checkpoint:  # For backwards compatability with v0_19
+        checkpoint = checkpoint['net']
+    for key, state_dict in checkpoint.items():
         assert key in model, key
         try:
             model[key].load_state_dict(state_dict)
